437753260d8473a279b8754cffadb7054562573d
shinthantequi@gmail.com <shinthantequi@gmail.com>
2022-05-05 05:14:17 +0630

    data seeding

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,8 +2,13 @@
 
 namespace Database\Seeders;
 
+use App\Models\Question;
+use App\Models\Tag;
+use App\Models\User;
+use Faker\Provider\Lorem;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
 
 class DatabaseSeeder extends Seeder
 {
@@ -14,6 +19,34 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // \App\Models\User::factory(10)->create();
+       User::create([
+           'name'=>'shinn thant',
+           'email'=>'shinthant@gmail.com',
+           'password'=>Hash::make('password'),
+           'image'=>'images/profile/default.png'
+       ]);
+
+       Tag::create([
+           'name'=>'Web Development',
+           'slug'=>'web development'
+       ]);
+
+       Tag::create([
+        'name'=>'Web Design',
+        'slug'=>'web design'
+       ]);
+
+       Tag::create([
+        'name'=>'Mobile Development',
+        'slug'=>'mobile development'
+       ]);
+
+       Question::create([
+           'user_id'=> 1,
+           'title'=>'what is wrong with inertia',
+           'description'=>'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry'
+       ]);
+
     }
 }
+

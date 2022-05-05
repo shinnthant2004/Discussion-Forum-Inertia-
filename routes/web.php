755c221863c0de74a7b2b03fb23f6fc576f14403
shinthantequi@gmail.com <shinthantequi@gmail.com>
2022-05-06 00:13:27 +0630

    route guard

--- routes/web.php
+++ routes/web.php
@@ -4,11 +4,13 @@ use App\Http\Controllers\AuthController;
 use App\Http\Controllers\PageController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/',[PageController::class,'home']);
+Route::middleware('auth')->group(function(){
+    Route::get('/',[PageController::class,'home']);
+});
 
-Route::get('/login',[AuthController::class,'login']);
-
-#Register
-Route::get('/regist',[AuthController::class,'register']);
-Route::post('/regist',[AuthController::class,'postRegister']);
+Route::middleware('guest')->group(function(){
+    Route::get('/login',[AuthController::class,'login'])->name('login');
+    Route::get('/regist',[AuthController::class,'register']);
+    Route::post('/regist',[AuthController::class,'postRegister']);
+});
 
